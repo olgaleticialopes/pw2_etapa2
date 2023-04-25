49b2ecc19efab84e776f4483389985a483e829da
olgaleticialopes <leticiaolgalopes@gmail.com>
2023-04-24 22:39:54 -0300

    Aula 2404 - Middleware e URL parametro

--- appExpress/index.js
+++ appExpress/index.js
@@ -5,11 +5,30 @@ const port = 3000
 const path = require('path')
 const basePath = path.join(__dirname, 'templates')
 
+var checkAuth = function(req, res, next){
+    req.authStatus = true
+    if(req.authStatus){
+        console.log('Está Logado, Prossiga!')
+        next()
+    }
+    else{
+        console.log('Não Logado! Faça o Login!')
+    }
+}
+
+app.use(checkAuth)
+
 app.get('/',(req,res)=>{
     res.sendFile(`${basePath}/index.html`)
 })
 
+app.get ('/user/:id', (req,res) =>{
+    let emailUser= req.params.id+"@gmail.com"
+    console.log( `parametro de usuario:${emailUser}`)
+res.sendFile(`${basePath}/user.html`)
+})
 
 app.listen(port, () => {
  console.log(`Servirdor rodando na porta: ${port}`)
 })
+
