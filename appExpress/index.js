711dcff99f2d9fc9074023dd74edfa609e7769db
olgaleticialopes <leticiaolgalopes@gmail.com>
2023-05-04 20:44:25 -0300

    Aula0405 Rotas e Middleware para controle API

--- appExpress/index.js
+++ appExpress/index.js
@@ -1,35 +1,27 @@
 const express = require('express')
-const { restart } = require('nodemon')
 const app = express()
 const port = 3000
 const path = require('path')
+const users = require('./user')
+const basePath = path.join(__dirname,'templates')
 
-const basePath = path.join(__dirname, 'templates')
+
 
 app.use(
+    //encodar abrir pacote URL
     express.urlencoded({
-        extended:true,
-}))
-
+        extended: true,
+    })
+)
 app.use(express.json())
+app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
+app.use('/users', users)
+
+app.get('/', (req, res)=>{
     res.sendFile(`${basePath}/index.html`)
 })
 
-app.get('/user/add', (req,res)=>{
-    res.sendFile(`${basePath}/userForm.html`)
-})
-
-app.post('/users/save', (req,res)=>{
-    let name = req.body.name
-    let age = req.body.age
-    
-    console.log(`Node do usuario: ${name}, Idade do usuario: ${age}`)
-    restart.sendFile(`${basePath}/index.html`)
-})
-
 app.listen(port, () => {
- console.log(`Servirdor rodando na porta: ${port}`)
+    console.log(`Servidor Rodando na porta: ${port}`)
 })
-
